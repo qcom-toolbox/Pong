2dce66a6034832d8be1b3abbd8c0a97fad01967f
qcom-toolbox <169756160+qcom-toolbox@users.noreply.github.com>
2026-07-10 13:19:46 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -19,6 +19,8 @@ size = (700,500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
 
+font = pygame.font.Font(None, 50)
+
 clock = pygame.time.Clock()
 
 player_1 = pygame.Rect(
@@ -38,6 +40,17 @@ player_2 = pygame.Rect(
 
 ball_speed_x = 5
 ball_speed_y = 5
+
+score_1 = 0
+score_2 = 0
+
+def reset_ball():
+    global ball_speed_x, ball_speed_y
+
+    ball.center = (350, 250)
+
+    ball_speed_x *= -1
+    ball_speed_y = 5
 
 running = True
 
@@ -78,13 +91,37 @@ while running:
 
     if ball.top <= 0 or ball.bottom >= 500:
         ball_speed_y *= -1
-
-    if ball.colliderect(player_1) or ball.colliderect(player_2):
-        ball_speed_x *= -1
         
+    if ball.colliderect(player_1):
+        ball.left = player_1.right
+        ball_speed_x *= -1
+
+    if ball.colliderect(player_2):
+        ball.right = player_2.left
+        ball_speed_x *= -1
+
+    if ball.left <=0:
+         score_2 += 1
+         reset_ball()
+
+    if ball.right >= 700:
+         score_1 += 1
+         reset_ball()
+
     screen.fill(BLACK)
 
     pygame.draw.rect(screen,WHITE,player_1)
+
+    score_text = font.render(
+         f"{score_1}     {score_2}",
+         True,
+         WHITE
+    )
+
+    screen.blit(score_text,(300,20))
+    
+
+
     pygame.draw.rect(screen,WHITE,player_2)
 
     pygame.draw.rect(screen,WHITE,ball)
