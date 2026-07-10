9461af31c7d10668341e3728eea0c61117cf381b
qcom-toolbox <169756160+qcom-toolbox@users.noreply.github.com>
2026-07-10 11:23:34 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -9,77 +9,82 @@ size = (700,500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong")
 
-while True:
-
-    for event in pygame.event.get():
-        if event.type ==  pygame.QUIT:
-            running = False
-
-pygame.quit()
-
-screen.fill(BLACK)
-
-pygame.display.update()
-
-player_width = 10
-Player_height = 100
+clock = pygame.time.Clock()
 
 player_1 = pygame.Rect(
 
     20,
     200,
-    player_width,
+    player_widht,
     Player_height
 )
 
 player_2 = pygame.Rect(
     670,
     200,
-    player_width,
+    player_widht,
     Player_height
-)
-
-pygame.draw.rect(screen,WHITE,player_1)
-pygame.draw.rect(screen,WHITE,player_2)
-
-keys = pygame.key.get_pressed()
-
-if keys[pygame.K_z]:
-    player_1.y -= 5
-
-if keys[pygame.K_s]:
-    player_1.y += 5
-    
-if keys[pygame.K_UP]:
-    player_2.y -= 5
-
-if keys[pygame.K_DOWN]:
-    player_2.y += 5
-
-    ball = pygame.Rect(
-        345,
-        245,
-        10,
-        10
 )
 
 ball_speed_x = 5
 ball_speed_y = 5
 
-ball.x += ball_speed_x
-ball.y += ball_speed_y
+while True:
 
-if ball.top <= 0 or ball.bottom >= 500:
-    ball_speed_y *= -1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-ball.colliderect(player_1)
 
-if ball.colliderect(player_1) or ball.colliderect(player_2):
-    ball_speed_x *= -1
+    screen.fill(BLACK)
 
-pygame.draw.rect(screen,WHITE,ball)
+    pygame.display.update()
 
-clock = pygame.time.Clock()
+    player_widht = 10
+    Player_height = 100
 
-clock.tick(60)
 
+
+    pygame.draw.rect(screen,WHITE,player_1)
+    pygame.draw.rect(screen,WHITE,player_2)
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_z]:
+        player_1.y -= 5
+
+    if keys[pygame.K_s]:
+        player_1.y += 5
+
+    if keys[pygame.K_UP]:
+        player_2.y -= 5
+
+    if keys[pygame.K_DOWN]:
+        player_2.y += 5
+
+        ball = pygame.Rect(
+            345,
+            245,
+            10,
+            10
+    )
+
+
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= 500:
+        ball_speed_y *= -1
+
+    ball.colliderect(player_1)
+
+    if ball.colliderect(player_1) or ball.colliderect(player_2):
+        ball_speed_x *= -1
+
+    pygame.draw.rect(screen,WHITE,ball)
+
+
+    clock.tick(60)
+
+    pygame.quit()
